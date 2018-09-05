bf76e75fb650c6e19d15780a58f5780abb37bc40
chenjiehao <chenjiehao@qianshengqian.com>
2018-09-05 19:23:45 +0800

    once

--- src/main/scala/com/jh/learn/flink/FlinkTest.scala
+++ src/main/scala/com/jh/learn/flink/FlinkTest.scala
@@ -1,8 +1,39 @@
-package com.jh.learn.flink
-
-/**
-  * Created by chenjiehao on 2018/9/5
-  */
-object FlinkTest {
-
-}
+//package com.jh.learn.flink
+//
+//import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment}
+//import org.apache.flink.table.api.TableEnvironment
+//import org.apache.flink.api.scala._
+//import org.apache.flink.table.api.scala._
+//
+///**
+//  * Created by chenjiehao on 2018/9/5
+//  */
+//object FlinkTest {
+//  case class Person(id:Int,name:String,age:Int)
+//  def main(args: Array[String]): Unit = {
+//
+//    // set up execution environment
+////    val env = ExecutionEnvironment.getExecutionEnvironment
+//    val env = ExecutionEnvironment.getExecutionEnvironment
+//    val tEnv = TableEnvironment.getTableEnvironment(env)
+//
+////    val tableEnvironment = TableEnvironment.getTableEnvironment(env)
+//    val result: DataSet[Person] = env.readTextFile("/Users/chenjiehao/Desktop/flinkTabel.txt").map(v => {
+//      val ops = v.split("")
+//      Person(Integer.parseInt(ops(0)), ops(1), Integer.parseInt(ops(2)))
+//    })
+//
+////    val table = tEnv.fromDataSet(result)
+//
+//
+////    tEnv.registerTable("test",table)
+//
+////    tEnv.sqlQuery("select * from test")
+////    tEnv.sqlQuery("select * from test").
+////    result.print()
+////    tEnv.registerDataSet("testTable",result)
+//
+////    tEnv.sqlQuery("select * from testTable").printSchema()
+//
+//  }
+//}
--- src/main/scala/com/jh/learn/util/Substring_index.scala
+++ src/main/scala/com/jh/learn/util/Substring_index.scala
@@ -1,8 +1,47 @@
 package com.jh.learn.util
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by chenjiehao on 2018/9/5
   */
 object Substring_index {
+  /**
+    *
+    * @param str   传递的字符串
+    * @param key   关键字符 主要要是使用转义如"\\."  "\\?"
+    * @param number 关键字出现的次数
+    * @return
+    */
+  def test(str:String,key:String,number:Int): String ={
+    var result=""
+    val r=str.split(key) //以关键字作为切割，生成数组
 
+    //验证关键字出现的次数
+    if(number>=r.size||Math.abs(number)>=r.size){
+      return "error"
+    }
+
+    //次数为正负的考虑
+    if(number>0){
+      for(i <- 0 until  number){
+        if(i==number-1){
+          result+=r(i)
+        }else{
+          result+=r(i)+key.replace("\\","")
+        }
+      }
+      return result
+    }else{
+      val array=ArrayBuffer[String]()
+      for(i <- ( r.size-Math.abs(number) to Math.abs(number)).reverse){
+        if(i==r.size-Math.abs(number)){
+          array+=r(i)
+        }else{
+          array+=key.replace("\\","")+r(i)
+        }
+      }
+      return array.reverse.mkString
+    }
+  }
 }
