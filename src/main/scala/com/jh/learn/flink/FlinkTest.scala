1b8df393e96a3f121fc03e39f806e5b33752a040
chenjiehao <chenjiehao@qianshengqian.com>
2018-09-05 19:20:28 +0800

    substring_index

---
+++ src/main/scala/com/jh/learn/flink/FlinkTest.scala
@@ -0,0 +1,8 @@
+package com.jh.learn.flink
+
+/**
+  * Created by chenjiehao on 2018/9/5
+  */
+object FlinkTest {
+
+}
---
+++ src/main/scala/com/jh/learn/util/Substring_index.scala
@@ -0,0 +1,8 @@
+package com.jh.learn.util
+
+/**
+  * Created by chenjiehao on 2018/9/5
+  */
+object Substring_index {
+
+}
